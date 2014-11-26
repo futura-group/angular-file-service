87f69f45bb0579b944e427e5a24d3da4f7fc2147
Chriest Yu <jcppman@gmail.com>
2014-11-26 18:44:03 +0800

    Sourcemap

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 angular service for File manipulation
 
+## Usage
+
+- 'npm install angular-file-service'
+- usable scripts are under your 'node_modules/angular-file-service/dist'
+
+**Important note**: this module is not in commonJS format
+
 ## Credit
 
 - base64 encode & decode is from [base64-bufferarray]()
--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,18 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('build', function () {
     return gulp.src([
         'node_modules/spark-md5/spark-md5.js',
         'angular-file-service.js'
     ])
-    .pipe(concat('angular-file-service.js'))
-    .pipe(gulp.dest('dist'))
-    .pipe(uglify())
+    .pipe(sourcemaps.init())
+        .pipe(concat('angular-file-service.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(uglify())
+    .pipe(sourcemaps.write())
     .pipe(rename({
         extname: '.min.js'
     }))
